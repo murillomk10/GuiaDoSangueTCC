6ab6c1f0e3514e47201fb02b93f988e40830eede
murillomk10 <cmcalapica@gmail.com>
2025-10-17 08:53:23 -0300

    subindo

--- frontend/src/pages/inicio.jsx
+++ frontend/src/pages/inicio.jsx
@@ -1,6 +1,11 @@
 import Cabecalho from '../components/cabecalho';
 import './inicio.scss';
 import imagemSlide1 from '../assets/image.png';
+import imagemSlide2 from '../assets/image2.png';
+import imagemSlide3 from '../assets/image3.png';
+import gotasSangue from '../assets/gotasS.svg';
+import coracao from '../assets/heart.svg';
+import comunidade from '../assets/people-roof.svg';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
@@ -11,9 +16,8 @@ import 'swiper/css/pagination';
 export default function Inicio() {
     const data = [
         { id: '1', imagem: imagemSlide1 },
-        { id: '2', imagem: 'https://swiperjs.com/demos/images/nature-2.jpg' },
-        { id: '3', imagem: 'https://swiperjs.com/demos/images/nature-3.jpg' },
-        { id: '4', imagem: 'https://swiperjs.com/demos/images/nature-4.jpg' },
+        { id: '2', imagem: imagemSlide2 },
+        { id: '3', imagem: imagemSlide3 },  
     ];
 
     return (
@@ -45,10 +49,33 @@ export default function Inicio() {
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <div className='Container-section'>
+            <div class="linha-vertical">
+                <p className='texto-principal'>
+                “O Guia do Sangue é um portal informativo que tem como missão <br /> orientar e incentivar pessoas a se tornarem doadoras de sangue. Nosso <br /> propósito é conectar solidariedade e informação, ajudando a salvar <br /> vidas por meio do conhecimento e da empatia.”
+                </p>
 
-            <div>
-                
+                <div className='Cartoes'>
+                    <div className='cartao'>
+                        <img src={gotasSangue} alt="gostas-de-sangue" />
+                        <p>Doe sangue para transformar solidariedade em vida.</p>
+                    </div>
+                    <div className='cartao'>
+                        <img src={coracao} alt="coracao" />
+                        <p>Determinando solidariedade com amor em cada gota.</p>
+                    </div>
+                    <div className='cartao'>
+                        <img src={comunidade} alt="comunidade" />
+                        <p>Doando vida, fortalecendo a comunidade.</p>
+                    </div>
+                </div>
+                <div className="faixa-preta"></div>
+            </div>
+
+
             </div>
         </div>
+
+        
     );
 }
